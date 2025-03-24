988a9ad6407fb155242e32e1a3b4278c4c31208e
feuerdoge <deniboz11@gmail.com>
2025-03-24 17:04:43 +0100

    Kommentare
    - Kommentare Hinzugefügt

--- autoritareFactory2/setup/Band.cs
+++ autoritareFactory2/setup/Band.cs
@@ -10,10 +10,10 @@ namespace factordictatorship.setup
 {
     public class Band : Building
     {
-        public List<Resource> resource { get; }
-        public int ItemAnzahlMax;
-        public int ItemAnzahlMoment;
-        public int BandGeschwindigkeit;
+        public List<Resource> resource { get; } 
+        public int ItemAnzahlMax; //Anzahl an Items, die ein Band maximal halten kann.
+        public int ItemAnzahlMoment; //Anzahl an Items, die sich gerade auf dem Band befinden.
+        public int BandGeschwindigkeit; //Wie schnell es Items von einem auf das andere Band befördern kann.
 
         public Band(int bandGe, int itemAnMax) 
         {
