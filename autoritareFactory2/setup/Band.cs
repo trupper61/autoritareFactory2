355e9f51df561dba34349e8c5022d6dbcc0eb19c
feuerdoge <deniboz11@gmail.com>
2025-03-24 17:03:14 +0100

    Erste Schritte
    - Klasse erstellt
    - Methoden definiert
    - Allgemeine Bandstruktur definiert

---
+++ autoritareFactory2/setup/Band.cs
@@ -0,0 +1,42 @@
+﻿using autoritaereFactory;
+using autoritaereFactory.setup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factordictatorship.setup
+{
+    public class Band : Building
+    {
+        public List<Resource> resource { get; }
+        public int ItemAnzahlMax;
+        public int ItemAnzahlMoment;
+        public int BandGeschwindigkeit;
+
+        public Band(int bandGe, int itemAnMax) 
+        {
+            BandGeschwindigkeit = bandGe;
+            ItemAnzahlMax = itemAnMax;
+            ItemAnzahlMoment = 0;
+        }
+
+        public void ErkenneRescourcen() 
+        {
+            if(resource != null) 
+            {
+                foreach (Resource r in resource) 
+                {
+                    ItemAnzahlMoment++;
+                }
+            }
+        }
+
+        public void RescourceKommtAufBand(Resource r) 
+        {
+            resource.Add(r);
+        }
+
+    }
+}
