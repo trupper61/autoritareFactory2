e4631ee7fab9d633b23f6e8a3be81f054ea0feb9
feuerdoge <deniboz11@gmail.com>
2025-04-03 11:08:42 +0200

    Transport++
    -Rescourcen von einem auf dem anderen Band transportierbar
    -Hotfixes
    -Neue Klassen für unterschiedliche
    Bandtypes erstellt

---
+++ autoritareFactory2/setup/BaenderTypes/CurveBand.cs
@@ -0,0 +1,19 @@
+﻿using autoritaereFactory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factordictatorship.setup.BaenderTypes
+{
+    //Curved Band Von Links nach Oben
+    public class CurveBand : Band
+    {
+        public CurveBand(int bandGe, int itemAnMax, List<Resource> resources, int richtung, int anzEisen, int positionX, int positionY) 
+            : base(bandGe, itemAnMax, resources, richtung, anzEisen, positionX, positionY)
+        {
+
+        }
+    }
+}
--- autoritareFactory2/setup/Band.cs
+++ autoritareFactory2/setup/Band.cs
@@ -1,10 +1,12 @@
 ﻿using autoritaereFactory;
 using autoritaereFactory.setup;
+using autoritaereFactory.world;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace factordictatorship.setup
 {
@@ -15,7 +17,7 @@ namespace factordictatorship.setup
         public int ItemAnzahlMax = 10; //Anzahl an Items, die ein Band maximal halten kann.
         public int ItemAnzahlMoment; //Anzahl an Items, die sich gerade auf dem Band befinden.
         public int BandGeschwindigkeit = 5; //Wie schnell es Items von einem auf das andere Band befördern kann. (Item Pro Sekunde)
-        public int Richtung; //0 -> Rechts||| 1 -> Unten||| 2 -> Links||| 3 -> Oben|||
+        public int Richtung; //1 -> Links nach Rechts||| 2 -> Oben nach Unten||| 3 -> Rechts nach Links||| 4 -> Unten nach Oben|||
 
         public Band (int bandGe, int itemAnMax, List<Resource> resources, int richtung, int anzEisen, int positionX, int positionY)
             :base(positionX, positionY) 
@@ -53,6 +55,50 @@ namespace factordictatorship.setup
             Resource r = resource[stelleInResourcedieGenommenWerdenSoll];
             resource.RemoveAt(stelleInResourcedieGenommenWerdenSoll);
             return r;
+        }
+        public void InNaechsteBand(Band band, Band BandNxt,WorldMap world) 
+        {
+            //Schaue alle benachbarten tiles an. Schaue wo ein Band ist. Wenn Band ist nehme die Richtung dieses Bandes. Wenn Bandrichtung gleich ist wie momentaner Band,
+            //Transfer rescourcen.
+            int wertRotX = 0;
+            int wertRotY = 0;
+
+            for(int i = 4; i > 0; i--) 
+            {
+                switch(i) 
+                {
+                    case 4:
+                        wertRotX = -1;
+                        break;
+                    case 3:
+                        wertRotX = 1;
+                        break;
+                    case 2:
+                        wertRotY = -1;
+                        wertRotX = 0;
+                        break;
+                    case 1:
+                        wertRotY = 1;
+                        break;
+                }
+                if(world.GetEntityInPos(band.PositionX + wertRotX, band.PositionY + wertRotY).Count == 1) 
+                {
+                    //Damit man die Richtung des Bandes nehmen kann, muss man zunächst das Gebäude von der Liste holen.
+                    foreach(Band gb in world.GetEntityInPos(band.PositionX + wertRotX, band.PositionY + wertRotY)) 
+                    {
+                        BandNxt = gb;
+                    }
+                    if(BandNxt.Richtung == band.Richtung) 
+                    {
+                        foreach(Resource resources in resource) 
+                        {
+                            BandNxt.RescourceKommtAufBand(resources);
+                            band.resource.Remove(resources);
+                        }
+                    }
+                }
+                
+            }
         }
 
     }
