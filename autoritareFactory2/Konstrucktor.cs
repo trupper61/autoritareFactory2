528f80db4a6bc09208145f3ca0c4d187005cc53f
Schueler der IT-Abteilung <Schueler-IT@JPRS.local>
2025-04-03 11:12:03 +0200

    Update 2

--- autoritareFactory2/Konstrucktor.cs
+++ autoritareFactory2/Konstrucktor.cs
@@ -31,11 +31,20 @@ namespace factordictatorship
         public int Produktionsdauer { get { return produktionsdauer; } }
         private int verbleibendeProduktionsdauer;//verbleibende dauer des Produktionsprozesses in millisekunden
         public int VerbleibendeProduktionsdauer { get { return verbleibendeProduktionsdauer; } }
-        private int d;
-        public Konstrucktor(int positionX, int positionY) : base(positionX, positionY)
+        private int drehung;//wert der Dreung 1: Eingang links, Ausgang rechts und dann im Urzeigersinn bis 4: Engang unten, Ausgang oben
+        public int Drehung { get { return drehung; } }
+        public Konstrucktor(int positionX, int positionY, int drehung) : base(positionX, positionY)
         {
+            this.drehung = drehung;
             längeInXRichtung = 2;
             längeInYRichtung = 1;
+        }
+        private void PassLängeZUDreungAn()
+        {
+            if (drehung%2 == 0)
+            {
+
+            }
         }
         public void SpeichereRezept(Rezepte gewähltesRezept)
         {
