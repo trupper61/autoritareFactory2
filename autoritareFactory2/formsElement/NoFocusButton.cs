6d34307aecb22a740db180feecb149ca7dee7a20
GoogolFisch <florian.losensky@yahoo.com>
2025-04-06 08:12:19 +0200

    EGERER-35 Now adding an Button, that works...

---
+++ autoritareFactory2/formsElement/NoFocusButton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace factordictatorship.formsElement
+{
+    public class NoFocusButton : Button
+    {
+
+        public NoFocusButton() : base()
+        {
+            SetStyle(ControlStyles.Selectable, false);
+        }
+    }
+}
--- autoritareFactory2/world.Designer.cs
+++ autoritareFactory2/world.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace factordictatorship
+﻿using factordictatorship.formsElement;
+using System.Windows.Forms;
+
+namespace factordictatorship
 {
     partial class world
     {
@@ -28,15 +31,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.noFocusButton = new NoFocusButton();
             this.SuspendLayout();
+            // 
+            // noSelButton
+            // 
+            this.noFocusButton.CausesValidation = false;
+            this.noFocusButton.Location = new System.Drawing.Point(271, 90);
+            this.noFocusButton.Name = "noFocusButton";
+            this.noFocusButton.Size = new System.Drawing.Size(100, 35);
+            this.noFocusButton.TabIndex = 0;
+            this.noFocusButton.Text = "Not Focusable Button";
+            this.noFocusButton.UseVisualStyleBackColor = true;
             // 
             // world
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.noFocusButton);
             this.DoubleBuffered = true;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "world";
             this.Text = "FactoriaAutoritaet";
             this.ResumeLayout(false);
@@ -44,6 +59,8 @@
         }
 
         #endregion
+
+        private Button noFocusButton;
     }
 }
 
