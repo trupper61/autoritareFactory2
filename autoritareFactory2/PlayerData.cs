05b3eaddef4151d425449d2328e2c784280b1976
feuerdoge <deniboz11@gmail.com>
2025-04-23 18:16:25 +0200

    PlayerData
    -Added Player Data Class
    -Placeholder Assets for money implementation

---
+++ autoritareFactory2/PlayerData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factordictatorship
+{
+    public class PlayerData
+    {
+        public int money;
+        public int level;
+
+        public PlayerData(int money, int level)
+        {
+            this.money = money;
+            this.level = level;
+        }
+    }
+}
