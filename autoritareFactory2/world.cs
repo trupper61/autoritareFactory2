81c859b71a87e66722a3aea4b56d8149fef89dfa
GoogolFisch <florian.losensky@yahoo.com>
2025-04-24 17:27:18 +0200

    Merge Error - Fixed

--- autoritareFactory2/world.cs
+++ autoritareFactory2/world.cs
@@ -186,19 +186,6 @@ namespace factordictatorship
                     }
                 }
             }
-            else
-            {
-                List<Fabrikgebeude> fab = mapWorld.GetEntityInPos(worldPoint.X, worldPoint.Y);
-                if (fab.Count == 0)
-                    return;
-                foreach (Fabrikgebeude f in fab)
-                {
-                    if (f is Konstrucktor)
-                    {
-                        ShowKonInterface(f as Konstrucktor);
-                    }
-                }
-            }
         }
         public void RefreshLoop(object sender, EventArgs e)
         {
