a463878d45f94f1cd032825908701badbf094933
feuerdoge <deniboz11@gmail.com>
2025-05-08 19:42:12 +0200

    Exporthaus class

---
+++ autoritareFactory2/Exporthaus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factordictatorship
+{
+    public class Exporthaus : Fabrikgebeude
+    {
+        public Exporthaus(int positionX, int positionY, int drehung) 
+            : base(positionX, positionY, drehung)
+        {
+
+        }
+
+        public override void Iteration() 
+        { 
+            
+        }
+
+
+    }
+}
