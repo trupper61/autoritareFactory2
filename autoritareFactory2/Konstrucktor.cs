29a4a81c1dca0f70021f71d5bd8e1c5f579f8a79
trupper61 <152274189+trupper61@users.noreply.github.com>
2025-05-09 10:58:00 +0200

    Konstruktor fix

--- autoritareFactory2/Konstrucktor.cs
+++ autoritareFactory2/Konstrucktor.cs
@@ -104,8 +104,8 @@ namespace factordictatorship
                 List<Fabrikgebeude> entitys = WorldMap.theWorld.GetEntityInPos(DrehePAufXAchse(verschiebungXAchse, verschiebungYAchse), DrehePAufYAchse(verschiebungXAchse, verschiebungYAchse));
                 if (entitys.Count == 1)
                 {
-                    if (entitys[0].GetType() == typeof(Band))
-                        return;
+                    //if (entitys[0].GetType() == typeof(Band))
+                    //    return;
                     Band band = (Band)entitys[0];
                     if (band != null)
                     {
@@ -127,8 +127,8 @@ namespace factordictatorship
                 List<Fabrikgebeude> entitys = WorldMap.theWorld.GetEntityInPos(DrehePAufXAchse(verschiebungXAchse, verschiebungYAchse), DrehePAufYAchse(verschiebungXAchse, verschiebungYAchse));
                 if (entitys.Count == 1)
                 {
-                    if (entitys[0].GetType() == typeof(Band))
-                        return;
+                    //if (entitys[0].GetType() == typeof(Band))
+                    //    return;
                     Band band = (Band)entitys[0];
                     if (band != null)
                     {
