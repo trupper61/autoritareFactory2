c648b3c75a663dcfa28c96afc81d41f329f9df26
PhiloiImpetus <florianlosensky2@gmail.com>
2025-05-20 15:23:31 +0200

    It's not that much faster! :(

--- autoritareFactory2/drawing/WorldPartImage.cs
+++ autoritareFactory2/drawing/WorldPartImage.cs
@@ -10,6 +10,9 @@ namespace factordictatorship.drawing
     {
         public bool markeRemove;
         public Bitmap map;
+        // does this do anything?
+        public Bitmap map2;
+        public Bitmap map3;
         public autoritaereFactory.world.Chunk chRef;
         public int chunkX, chunkY;
         public WorldPartImage(int WorldPosX,int WorldPosY)
@@ -18,12 +21,18 @@ namespace factordictatorship.drawing
             chunkY = WorldPosY;
             chRef = WorldMap.theWorld.GetSpecificChunk(chunkX, chunkY);
             map = new Bitmap(ResourceHandler.imageSize * Chunk.chunkSize, ResourceHandler.imageSize * Chunk.chunkSize);
+            // does this do anything?
+            map2 = new Bitmap(ResourceHandler.imageSize * Chunk.chunkSize / 2, ResourceHandler.imageSize * Chunk.chunkSize / 2);
+            map3 = new Bitmap(ResourceHandler.imageSize * Chunk.chunkSize / 4, ResourceHandler.imageSize * Chunk.chunkSize / 4);
             ReGenerate();
             markeRemove = false;
         }
         public void ReGenerate()
         {
             Graphics grp = Graphics.FromImage(map);
+            // does this do anything?
+            Graphics grp2 = Graphics.FromImage(map2);
+            Graphics grp3 = Graphics.FromImage(map3);
             for (int ptX = 0; ptX < Chunk.chunkSize; ptX++)
             {
                 for (int ptY = 0; ptY < Chunk.chunkSize; ptY++)
@@ -34,13 +43,32 @@ namespace factordictatorship.drawing
                         ptX * ResourceHandler.imageSize, ptY * ResourceHandler.imageSize,
                         ResourceHandler.imageSize, ResourceHandler.imageSize)
                     );
+                    // does this do anything?
+                    grp2.DrawImage(
+                    ResourceHandler.tileSet[chRef.GetSubChunk(ptX, ptY)],
+                    new Rectangle(
+                        ptX * ResourceHandler.imageSize / 2, ptY * ResourceHandler.imageSize / 2,
+                        ResourceHandler.imageSize / 2, ResourceHandler.imageSize / 2)
+                    );
+                    grp3.DrawImage(
+                    ResourceHandler.tileSet[chRef.GetSubChunk(ptX, ptY)],
+                    new Rectangle(
+                        ptX * ResourceHandler.imageSize / 4, ptY * ResourceHandler.imageSize / 4,
+                        ResourceHandler.imageSize / 4, ResourceHandler.imageSize / 4)
+                    );
                 }
             }
             grp.Dispose();
+            // does this do anything?
+            grp2.Dispose();
+            grp3.Dispose();
         }
         public void Dispose()
         {
             map.Dispose();
+            // does this do anything?
+            map2.Dispose();
+            map3.Dispose();
         }
     }
 }
