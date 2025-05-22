74fe7306a1e682f8ef412b620d29521d77ff4e19
trupper61 <152274189+trupper61@users.noreply.github.com>
2025-05-22 08:39:45 +0200

    added concreteBeam, finishinator fix

--- autoritareFactory2/Resources/ResourceHandler.cs
+++ autoritareFactory2/Resources/ResourceHandler.cs
@@ -60,6 +60,7 @@ namespace factordictatorship.Resources
             {ResourceType.SteelIngot,LoadImage("item\\StealIngotAsItem.png") },
             {ResourceType.SteelRod,LoadImage("item\\StealRodAsItem.png") },
             {ResourceType.SteelBeam,LoadImage("item\\StealBeam.png") },
+            {ResourceType.SteelConcreteBeam,LoadImage("item\\StealBeamWithConcrete.png") },
         };
         public const int imageSize = 44;
         public static Image LoadImage(string path)
